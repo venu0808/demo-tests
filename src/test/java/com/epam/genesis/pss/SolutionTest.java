0b2e335ab08b713839b23c329bfdff9decdabaa9
Venu_kandagatla <venu_kandagatla@epam.com>
2022-11-03 17:57:46 +0530

    Test repo
    Changed

---
+++ src/test/java/com/epam/genesis/pss/SolutionTest.java
@@ -0,0 +1,11 @@
+package com.epam.genesis.pss;
+
+import org.junit.jupiter.api.Test;
+
+
+public class SolutionTest {
+    @Test
+    public void test1() {
+        Solution solution = new Solution();
+    }
+}
